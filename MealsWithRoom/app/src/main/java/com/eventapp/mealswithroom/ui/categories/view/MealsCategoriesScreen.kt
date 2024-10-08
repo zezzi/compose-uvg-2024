8664c5aa0a2caef10ee456b15f1e32740e0b4b4c
Ceci Castillo <>
2024-10-07 20:44:11 -0600

    Add a Loading Spinner

--- MealsWithRoom/app/src/main/java/com/eventapp/mealswithroom/ui/categories/view/MealsCategoriesScreen.kt
+++ MealsWithRoom/app/src/main/java/com/eventapp/mealswithroom/ui/categories/view/MealsCategoriesScreen.kt
@@ -1,20 +1,25 @@
 package com.eventapp.mealswithroom.ui.categories.view
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.eventapp.mealswithroom.ui.categories.viewmodel.MealsCategoriesViewModel
 import com.eventapp.mealswithroom.navigation.AppBar
+import com.eventapp.mealswithroom.ui.categories.viewmodel.MealsCategoriesViewModel
 import com.eventapp.mealswithroom.ui.theme.MealsWithRoomTheme
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -22,6 +27,7 @@ import com.eventapp.mealswithroom.ui.theme.MealsWithRoomTheme
 fun MealsCategoriesScreen(navController: NavController,
                           viewModel: MealsCategoriesViewModel = viewModel()) {
     val categories = viewModel.categories.observeAsState(initial = emptyList())
+    val isLoading = viewModel.isLoading.observeAsState(initial = false)
 
     LaunchedEffect(Unit) {
         viewModel.fetchCategories()
@@ -30,9 +36,19 @@ fun MealsCategoriesScreen(navController: NavController,
     Scaffold(topBar = {
         AppBar(title = "Recipes", navController = navController)
     }) {
-        LazyColumn(contentPadding = PaddingValues(16.dp)) {
-            items(categories.value) { category ->
-                MealCategory(category, navController)
+        Box(modifier = Modifier.fillMaxSize()) {
+            if (isLoading.value) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            } else {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                ) {
+                    items(categories.value) { category ->
+                        MealCategory(category, navController)
+                    }
+                }
             }
         }
     }
--- MealsWithRoom/app/src/main/java/com/eventapp/mealswithroom/ui/categories/viewmodel/MealsCategoriesViewModel.kt
+++ MealsWithRoom/app/src/main/java/com/eventapp/mealswithroom/ui/categories/viewmodel/MealsCategoriesViewModel.kt
@@ -13,11 +13,15 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MealsCategoriesViewModel(private val repository: MealsCategoryRepository = MealsCategoryRepository()): ViewModel() {
-    
+
     private val _categories = MutableLiveData<List<Categories>>()
     val categories: LiveData<List<Categories>> = _categories
 
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
     fun fetchCategories() {
+        _isLoading.value = true
         Log.d("TAG_COROUTINES", "about to launch a coroutine")
         viewModelScope.launch(Dispatchers.IO) {
             Log.d("TAG_COROUTINES", "launching a coroutine")
@@ -28,6 +32,9 @@ class MealsCategoriesViewModel(private val repository: MealsCategoryRepository =
             } catch (e: Exception) {
                 // Handle any errors here
                 e.printStackTrace()
+            } finally {
+                // Set loading to false after data is fetched
+                _isLoading.postValue(false)
             }
         }
         Log.d("TAG_COROUTINES", "other work")
